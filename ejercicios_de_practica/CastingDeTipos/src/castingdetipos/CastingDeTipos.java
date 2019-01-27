e47a03ac895877f03d95e6efbab1593485654046
Claudio Altamiranda <claudioaltamiranda@yahoo.com.ar>
2019-01-26 21:34:13 -0300

    clase 2

---
+++ ejercicios_de_practica/CastingDeTipos/src/castingdetipos/CastingDeTipos.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package castingdetipos;
+
+/**
+ *
+ * @author claudioaltamiranda
+ */
+public class CastingDeTipos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int iNumeroUno = 10;
+        int iNumeroDos = 3;
+        float fNumero;
+        
+        fNumero = (float) (iNumeroUno / iNumeroDos);
+        
+        System.out.println(fNumero);
+    }
+    
+}
---
+++ ejercicios_de_practica/NumerosDePuntoFlotante/src/numerosdepuntoflotante/NumerosDePuntoFlotante.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numerosdepuntoflotante;
+
+/**
+ *
+ * @author claudioaltamiranda
+ */
+public class NumerosDePuntoFlotante {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int iNumeroUno = 10;
+        int iNumeroDos = 3;
+        float fNumero;
+        
+        fNumero = iNumeroUno / iNumeroDos;
+        
+        System.out.println(iNumeroUno);
+        System.out.println(iNumeroDos);
+        System.out.println(fNumero);
+    }
+    
+}
